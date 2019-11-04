eb5b1886c423c7b61c556103f34d49c043985187
ymsk.sky <ymsk.sky.95@gmail.com>
2019-11-04 21:14:06 +0900

    ヘッダーフッター実装

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -4,16 +4,16 @@
       Picshare
     </RouterLink>
     <div class="navbar__menu">
-      <div class="navbar_item">
+      <div v-if="isLogin" class="navbar_item">
         <button class="button">
           <i class="icon ion-md-add"></i>
           Submit a photo
         </button>
       </div>
-      <span class="navbar_item">
-        username
+      <span v-if="isLogin" class="navbar_item">
+        {{ username }}
       </span>
-      <div class="navbar_item">
+      <div v-else class="navbar_item">
         <RouterLink class="button button--link" to="/login">
           Login / Register
         </RouterLink>
@@ -21,3 +21,16 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLogin () {
+      return this.$store.getters['auth/check']
+    },
+    username () {
+      return this.$store.getters['auth/username']
+    }
+  }
+}
+</script>
