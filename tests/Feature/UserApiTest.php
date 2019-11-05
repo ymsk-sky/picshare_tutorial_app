4182328a6818e198e46d4ebce5fbffd34a44e729
ymsk.sky <ymsk.sky.95@gmail.com>
2019-11-05 21:49:46 +0900

    認証状態の維持を実装

---
+++ tests/Feature/UserApiTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // テストユーザー作成
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function should_return_logged_in_user()
+    {
+        $response = $this->actingAs($this->user)->json('GET', route('user'));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'name' => $this->user->name,
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function should_return_as_not_logged_in()
+    {
+        $response = $this->json('GET', route('user'));
+
+        $response->assertStatus(200);
+        $this->assertEquals("", $response->content());
+    }
+}
