8761bea599032bf083c698695bbdb68a8dc3496e
ymsk.sky <ymsk.sky.95@gmail.com>
2019-11-23 23:37:32 +0900

    詳細ページ実装

---
+++ tests/Feature/PhotoDetailApiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Photo;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PhotoDetailApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function should_return_correct_json()
+    {
+        factory(Photo::class)->create();
+        $photo = Photo::first();
+
+        $response = $this->json('GET', route('photo.show', [
+            'id' => $photo->id,
+        ]));
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'id' => $photo->id,
+                'url' => $photo->url,
+                'owner' => [
+                  'name' => $photo->owner->name,
+                ],
+            ]);
+    }
+}
